2f24ed30dd26d6d5ef67f141be933c1a37c908df
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-28 20:14:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,4 +17,9 @@ class Artist
   def songs
     Song.all.select {|song| song.artist == self}
   end
+  
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end
 end
